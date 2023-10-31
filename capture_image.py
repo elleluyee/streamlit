44bdd02362de93767697cc54bcf302201ca34915
elleluyee <58732839+elleluyee@users.noreply.github.com>
2023-10-31 19:54:50 +0800

    Add files via upload

---
+++ capture_image.py
@@ -0,0 +1,4 @@
+# from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
+import streamlit as st
+
+file_image = st.camera_input(label = "")
